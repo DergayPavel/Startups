69201c9cbde2cda2bde10c5310cf4a9a3c5166d8
Pavel Dergay <pavel.dergay@mail.ru>
2023-11-22 18:59:06 +0300

    fix: add form

--- sample/src/form/form.js
+++ sample/src/form/form.js
@@ -5,25 +5,29 @@ import Subtract from '../images/Subtract.png'
 
 function Form() {
   return (
-    <>
-        <div className='formBox'>
-            <div className='formBox-info'>
-                <div className='formBox-info_name'>
-                    Are you ready <br/> to board this rocket ship?
-                </div>
-                <div className='formBox-info_text'>
-                    Share your excitement with us.
-                </div>
-                <div className='formBox-info_form'>
-                    <input type='text' className='form_name'/>
-                    <input type="email" className='form_email'/>
-                    <input type="text" className='form_message'/>
-                    <div className='form_btn'>
-                        Shoot us a message
+    < >
+        <div className='form'>
+            <div className='formBox'>
+                <div className='formBox-info'>
+                    <div className='formBox-info_name'>
+                        Are you ready <br/> to board this rocket ship?
+                    </div>
+                    <div className='formBox-info_text'>
+                        Share your excitement with us.
+                    </div>
+                    <div className='formBox-info_form'>
+                        <form>
+                            <input type='text' className='form_name' placeholder='Name*'/>
+                            <input type="email" className='form_email' placeholder='Email*'/>
+                            <input type="text" className='form_message' placeholder='Message*'/>
+                            <div className='form_btn'>
+                                Shoot us a message
+                            </div> 
+                        </form>
                     </div>
                 </div>
+                <img src={Subtract} alt='Subtract' className='formBox_img'/>
             </div>
-            <img src={Subtract} alt='Subtract' className='formBox_img'/>
         </div>
     </>
   ) ;
